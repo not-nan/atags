8289e68b8a467ecb2b4906cf312c4998664ff0f9
Saya <nnschr+github@protonmail.com>
2025-02-22 23:15:47 -0300

    Login add title

--- frontend/src/screens/Login.tsx
+++ frontend/src/screens/Login.tsx
@@ -38,57 +38,60 @@ const LoginScreen = () => {
 
   return (
     <div class="flex justify-center">
-      <div class="shrink rounded-xl border border-gray-400 dark:border-theme-pink px-2 py-4 my-5">
-        <select 
-        class="block mx-auto rounded-lg border disabled:text-gray-400 border-gray-400 dark:border-theme-pink p-2 mb-2"
-        onChange={(e) => setAuthType(e.target.value as 'oauth' | 'password')}>
-          <option value="oauth">OAuth</option>
-          <option value="password">App Password</option>
-        </select>
-        <div class="flex justify-between">
-          <label class="px-2 py-1 my-1">Handle or did</label>
-          <input
-            id="didOrHandle"
-            class="rounded-lg border disabled:text-gray-400 border-gray-400 dark:border-theme-pink px-2 py-1 my-1 focus:dark:border-light-pink dark:focus:outline-none"
-            disabled={loading()}
-            onKeyDown={(ev) => 
-              ev.key === 'Enter' 
-              ? (authType() === 'password' ? passwordInputElement?.focus() : login())
-              : undefined}
-            onInput={(e) => setDidInput(e.currentTarget.value)}
-          />
-        </div>
-        <Show when={authType() === 'password'}>
+      <div>
+        <p class="text-2xl text-center font-semibold mt-3">📌 Boards</p>
+        <div class="shrink rounded-xl border border-gray-400 dark:border-theme-pink px-2 py-4 my-5">
+          <select
+            class="block mx-auto rounded-lg border disabled:text-gray-400 border-gray-400 dark:border-theme-pink p-2 mb-2"
+            onChange={(e) => setAuthType(e.target.value as 'oauth' | 'password')}>
+            <option value="oauth">OAuth</option>
+            <option value="password">App Password</option>
+          </select>
           <div class="flex justify-between">
-            <label class="px-2 py-1 my-1">App password</label>
+            <label class="px-2 py-1 my-1">Handle or did</label>
             <input
-              id="password"
-              type="password"
-              ref={passwordInputElement}
+              id="didOrHandle"
               class="rounded-lg border disabled:text-gray-400 border-gray-400 dark:border-theme-pink px-2 py-1 my-1 focus:dark:border-light-pink dark:focus:outline-none"
               disabled={loading()}
-              onKeyDown={(ev) => ev.key === 'Enter' ? login() : undefined}
-              onInput={(e) => setPasswordInput(e.currentTarget.value)}
+              onKeyDown={(ev) =>
+                ev.key === 'Enter'
+                  ? (authType() === 'password' ? passwordInputElement?.focus() : login())
+                  : undefined}
+              onInput={(e) => setDidInput(e.currentTarget.value)}
             />
           </div>
-        </Show>
-        <Show when={error}>
-          <p class="text-center text-red-500 my-2">{error()}</p>
-        </Show>
-        <div class="flex justify-center mt-2">
-          <button
-            disabled={loading()}
-            class="cursor-pointer mx-auto rounded-lg border disabled:text-gray-400 border-gray-400 dark:border-theme-pink font-bold px-2 py-1 hover:bg-gray-100 dark:hover:bg-darkish-pink"
-            onClick={() => login()}>
-            <Switch>
-              <Match when={loading()}>
-                Loading...
-              </Match>
-              <Match when={!loading()}>
-                Login
-              </Match>
-            </Switch>
-          </button>
+          <Show when={authType() === 'password'}>
+            <div class="flex justify-between">
+              <label class="px-2 py-1 my-1">App password</label>
+              <input
+                id="password"
+                type="password"
+                ref={passwordInputElement}
+                class="rounded-lg border disabled:text-gray-400 border-gray-400 dark:border-theme-pink px-2 py-1 my-1 focus:dark:border-light-pink dark:focus:outline-none"
+                disabled={loading()}
+                onKeyDown={(ev) => ev.key === 'Enter' ? login() : undefined}
+                onInput={(e) => setPasswordInput(e.currentTarget.value)}
+              />
+            </div>
+          </Show>
+          <Show when={error}>
+            <p class="text-center text-red-500 my-2">{error()}</p>
+          </Show>
+          <div class="flex justify-center mt-2">
+            <button
+              disabled={loading()}
+              class="cursor-pointer mx-auto rounded-lg border disabled:text-gray-400 border-gray-400 dark:border-theme-pink font-bold px-2 py-1 hover:bg-gray-100 dark:hover:bg-darkish-pink"
+              onClick={() => login()}>
+              <Switch>
+                <Match when={loading()}>
+                  Loading...
+                </Match>
+                <Match when={!loading()}>
+                  Login
+                </Match>
+              </Switch>
+            </button>
+          </div>
         </div>
       </div>
     </div>
